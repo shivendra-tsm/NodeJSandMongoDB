28cff9b3c3ba22581bbdc59dbed1f951ccbee6ef
Shivendra Singh Chandel <chandel486670@gmail.com>
2025-09-30 19:06:43 +0530

    assignment2-3

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,5 +1,7 @@
 const ErrorHandler = require("../middleware/ErrorHandlingClass");
 const blogSchema = require("../models/blogModel")
+const fs = require("fs");
+const path = require("path")
 
 const saveBlog = async(req, res, next) => {
     try{
@@ -32,6 +34,117 @@ const saveBlog = async(req, res, next) => {
 
 }
 
+
+const getBlogs = async(req,res,next) =>{
+    try{
+        const blogs = await blogSchema.find().populate("author","name -_id");
+        if(blogs.length === 0) {
+            return next(new ErrorHandler("No blog found",404));
+        }
+        return res.status(200).json({
+            success:true,
+            message:"blog fetched successfully",
+            data:blogs
+        })
+    } catch(err) {
+        return next(new ErrorHandler(err.message || "Internal server error",500));
+    }
+}
+
+
+const getBlogById = async(req,res,next) => {
+    try{
+        const id = req.id;
+        const blogs = await blogSchema.find({author:id});
+        if(blogs.length === 0) {
+            return next(new ErrorHandler("No blogs found",404));
+        }
+        return res.status(200).json({
+            success:true,
+            message:"blogs fetched",
+            data:blogs
+        })
+    }catch(err) {
+        return next(new ErrorHandler(err.message || "Internal server error",500));
+    }
+}
+
+const updateBlog = async(req,res,next) => {
+
+    try{
+
+        const {title, content} = req.body;
+        const {id} = req.params;
+        const author_id = req.id;
+
+        const blog = await blogSchema.findById(id);
+        if(!blog) {
+            return next(new ErrorHandler("No blog found",404));
+        }
+
+         if(author_id != blog.author) {
+            return next(new ErrorHandler("Invalid access",401));
+         }
+
+        if (req.file && blog.imgUrl) {
+          const oldImagePath = path.join("uploads", blog.imgUrl);
+          fs.unlink(oldImagePath, (err) => {
+            if (err) console.error("Failed to delete old image:", err);
+          });
+        }
+
+        blog.title = title || blog.title;
+        blog.content = content || blog.content;
+        blog.imgUrl = req.file ? req.file.filename : blog.imgUrl;
+
+        await blog.save()
+        return res.status(200).json({
+            success:true,
+            message:"blog updated successfully",
+            data:blog
+        })
+
+    }catch(err) {
+        return next(new ErrorHandler(err.message || "Internal server error",500));
+    }
+}
+
+
+const deleteBlog = async(req,res, next) => {
+    try {
+        const {id} = req.params;
+        const author_id = req.id;
+        const blog = await blogSchema.findById(id);
+        if(!blog) {
+            return next(new ErrorHandler("No blog found",404));
+        }
+         
+         if(author_id != blog.author) {
+            return next(new ErrorHandler("Invalid access",401));
+         }
+
+        if (blog.imgUrl) {
+          const oldImagePath = path.join("uploads", blog.imgUrl);
+          fs.unlink(oldImagePath, (err) => {
+            if (err) console.error("Failed to delete old image:", err);
+          });
+        }
+
+        await blogSchema.findByIdAndDelete(id);
+        return res.status(200).json({
+            success:true,
+            message:"blog deleted"
+        })
+        
+    }catch(err) {
+        return next(new ErrorHandler(err.message || "Internal server error",500));
+    }
+}
+
 module.exports = {
-    saveBlog
+    saveBlog,
+    getBlogs,
+    getBlogById,
+    updateBlog,
+    deleteBlog
 }
--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { HelloFun, Register, Login } = require("./controller/userController");
-const { saveBlog } = require("./controller/blogController");
+const { saveBlog, getBlogs, getBlogById, updateBlog, deleteBlog } = require("./controller/blogController");
 const authMiddleware = require("./middleware/authMiddleware");
 const upload = require("./FileUpload");
 const route = express.Router();
@@ -12,6 +12,13 @@ route.post("/register",Register);
 route.post("/login",Login);
 
 
+
 route.post("/saveblog",authMiddleware,upload.single("img"),saveBlog);
+route.get("/getblogs",authMiddleware,getBlogs);
+route.get("/getblogbyid",authMiddleware,getBlogById);
+route.put("/updateblog/:id",authMiddleware,upload.single("img"),updateBlog);
+route.delete("/deleteblog/:id",authMiddleware,deleteBlog);
+
+
 
 module.exports=route
